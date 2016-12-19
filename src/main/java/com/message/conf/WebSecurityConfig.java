8384e7f5a2bbc15b222feeecc9c3b1251d389df7
Lex <yobdc.101@gmail.com>
2016-12-19 14:12:19 +0800

    访客页面不需要登录

--- src/main/java/com/message/conf/WebSecurityConfig.java
+++ src/main/java/com/message/conf/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/home", "/guest").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
