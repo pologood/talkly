5ea1ea48d150a9430e25c983170d48709113a7d9
Lex <yobdc.101@gmail.com>
2016-12-28 17:20:59 +0800

    过滤guest访问api

--- src/main/java/com/message/conf/WebSecurityConfig.java
+++ src/main/java/com/message/conf/WebSecurityConfig.java
@@ -25,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                        "/",
                         "/home",
                         "/guest",
+                        "/api/agents",
                         "/chat/**",
                         "/amaze/**",
                         "/arclist/**"
